641cb8f3af13dc5bb6c110fbdfd17be137fcfabe
taijusanagi <taijusanagi@gmail.com>
2022-10-26 19:19:59 +0900

    add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { ThirdwebProvider } from "@thirdweb-dev/react/solana";
 import { Network } from "@thirdweb-dev/sdk/solana";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
 
-// Change the network to the one you want to use: "mainnet-beta", "testnet", "devnet", "localhost" or your own RPC endpoint
-const network: Network = "mainnet-beta";
+const network: Network = "devnet";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider network={network}>
       <WalletModalProvider>
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </WalletModalProvider>
     </ThirdwebProvider>
   );
